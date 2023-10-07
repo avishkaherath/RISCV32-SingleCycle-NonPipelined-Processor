6059bc5718ce1d4c9c5d5692a528a41706781a8e
avishkaherath <116508849+avishkaherath@users.noreply.github.com>
2023-10-08 01:02:44 +0530

    PC and PC_tb

---
+++ RISCV32I-SingleCycleProcessor.srcs/sim_1/new/PC_tb.sv
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/08/2023 12:17:33 AM
+// Design Name: 
+// Module Name: PC_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module PC_tb;
+
+    // Inputs
+    logic clk;
+    logic reset;
+    logic branch;
+    logic [31:0] branch_offset;
+
+    // Outputs
+    logic [31:0] pc_out;
+
+    // Instantiate the ProgramCounter module
+    PC uut (
+        .clk(clk),
+        .reset(reset),
+        .branch(branch),
+        .branch_offset(branch_offset),
+        .pc_out(pc_out)
+    );
+
+    // Clock generation
+    always begin
+        clk = 0;
+        #5;
+        clk = 1;
+        #5;
+    end
+
+    // Test stimulus
+    initial begin
+        // Initialize inputs
+        reset = 1;
+        branch = 0;
+        branch_offset = 0;
+
+        // Wait for a few clock cycles
+        #10;
+        reset = 0;
+
+        // Test case 1: Normal PC increment
+        #10;
+        assert(pc_out == 32'h00000004) else $display("Test case 1 failed");
+
+        // Test case 2: Branch with positive offset
+        branch = 1;
+        branch_offset = 4;
+        #10;
+        assert(pc_out == 32'h00000008) else $display("Test case 2 failed");
+
+        // Test case 3: Branch with negative offset
+        branch = 1;
+        branch_offset = -8;
+        #10;
+        assert(pc_out == 32'h00000000) else $display("Test case 3 failed");
+        
+        #10;
+
+        $finish;
+    end
+
+endmodule
---
+++ RISCV32I-SingleCycleProcessor.srcs/sources_1/new/PC.sv
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/07/2023 10:55:02 PM
+// Design Name: 
+// Module Name: PC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC (
+    input logic clk,
+    input logic reset,
+    input logic branch,
+    input logic [31:0] branch_offset,
+    output logic [31:0] pc_out
+);
+
+    // Register to hold the program counter
+    logic [31:0] pc_reg;
+
+    always_ff @(posedge clk or posedge reset) begin
+        logic [31:0] branch_offset_temp;
+
+        if (reset)
+            pc_reg <= 32'h00000000;  // Reset the PC to 0
+        else if (branch) begin
+            branch_offset_temp = $signed(branch_offset); // Convert to signed value
+            branch_offset_temp = {{31{branch_offset_temp[31]}}, branch_offset_temp}; // Sign-extend to 32 bits
+            branch_offset_temp = branch_offset_temp << 2; // Word Addressing (maintain boundary)
+            pc_reg <= pc_reg + branch_offset_temp;   // Branch taken, offset to PC
+        end
+        else
+            pc_reg <= pc_reg + 4;   // Increment PC by 4 for the next instruction
+    end
+
+    assign pc_out = pc_reg;
+
+endmodule
+
