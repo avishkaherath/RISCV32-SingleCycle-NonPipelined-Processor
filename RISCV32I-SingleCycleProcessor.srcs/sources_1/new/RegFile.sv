29fd0b5cad71ffff59f5b06ad48e3e6a0395d24d
avishkaherath <116508849+avishkaherath@users.noreply.github.com>
2023-10-09 00:51:33 +0530

    RegFile

---
+++ RISCV32I-SingleCycleProcessor.srcs/sources_1/new/RegFile.sv
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/09/2023 12:18:42 AM
+// Design Name: 
+// Module Name: RegFile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RegFile
+(
+    input logic [4:0]  read_register_1, // Read Register 1 index
+    input logic [4:0]  read_register_2, // Read Register 2 index
+    input logic [4:0]  write_register,  // Write Register index
+    input logic write_enable,    // Write enable signal
+    input logic [31:0] write_data,      // Data to be written
+    output logic [31:0] read_data_1,     // Data read from Register 1
+    output logic [31:0] read_data_2      // Data read from Register 2
+);
+
+    // Declare an array to store the register values
+    logic [31:0] registers [31:0];
+
+    // Read data from the registers
+    assign read_data_1 = (read_register_1 == 5'b0) ? 32'h0 : registers[read_register_1];
+    assign read_data_2 = (read_register_2 == 5'b0) ? 32'h0 : registers[read_register_2];
+
+    // Write data to the registers
+    always_ff @(posedge clk or posedge rst_n) begin
+        if (!rst_n) begin
+            // Asynchronous reset
+            registers <= '0;
+        end else if (write_enable) begin
+            if (write_register != 5'b0) begin
+                registers[write_register] <= write_data;
+            end
+        end
+    end
+
+endmodule
