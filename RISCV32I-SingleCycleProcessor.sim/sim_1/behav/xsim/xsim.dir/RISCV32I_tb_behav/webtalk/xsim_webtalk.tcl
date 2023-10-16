67f57614b695f8c0d68e2b810bbda92be80cd88b
avishkaherath <116508849+avishkaherath@users.noreply.github.com>
2023-10-16 23:59:13 +0530

    Edit 13

--- RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/xsim_webtalk.tcl
+++ RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Oct 16 19:47:28 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Oct 16 23:13:11 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2021.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3247384" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "1850f26c-0830-480f-a9c4-d5dd7bde917c" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "cafeb44a0f2545c580f910f7c9eb83eb" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i5-1135G7 @ 2.40GHz" -context "user_environment"
@@ -28,6 +28,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "90 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8364_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4228509940 -regid "" -xml C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8448_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 4045024574 -regid "" -xml C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/acham/Documents/GitHub/RISCV32-SingleCycle-NonPipelined-Processor/RISCV32I-SingleCycleProcessor.sim/sim_1/behav/xsim/xsim.dir/RISCV32I_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
